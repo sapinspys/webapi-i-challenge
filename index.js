20777c36911031c40e9caf217adf42e9fbd0909f
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-17 14:43:37 -0400

    Installed Express and begun scaffolding API file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+// implement your API here:
+
+// Using require() to import express module:
+// ES2015: 
+  // import express from 'express';
+const express = require('express');
+
+// Creates an express application (used to configure server):
+const server = express();
+
+// Setting up GET route handler function:
