2f2492dc2a2ddd9eaaaf2178ea953eef069345d8
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-17 15:14:34 -0400

    Added listen method to server

--- index.js
+++ index.js
@@ -4,8 +4,20 @@
 // ES2015: 
   // import express from 'express';
 const express = require('express');
+const db = require('./data/db');
 
 // Creates an express application (used to configure server):
 const server = express();
 
-// Setting up GET route handler function:
+// Setting up route handler functions on endpoints:
+
+// POST: Creates a user using the information sent inside the `request body`.
+// server.post(`/api/users`, )
+
+// GET: returns an array of all the user objects contained in the database. 
+// server.get('/api/users', (req, res) => {
+
+// })
+
+// Turning on the server:
+server.listen(8000, () => console.log('API running on port 8000'))
