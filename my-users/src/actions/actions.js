e66d5550d865b1b3c3fd11173f630f039ba3a45c
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-18 16:23:08 -0400

    Created action type and creator for getting all users

---
+++ my-users/src/actions/actions.js
@@ -0,0 +1,21 @@
+export const GET_USERS = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+export const getAllUsers = () => dispatch => {
+  dispatch ({ type: GET_USERS });
+  axios
+    .get('http://localhost:5000/api/users')
+    .then(res => {
+      dispatch({ 
+        type: SUCCESS, 
+        payload: res.data
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: FAILURE,
+        payload: err.response
+      })
+    })
+}
