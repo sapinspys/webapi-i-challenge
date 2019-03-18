e3e7a10f6ee781cbcf1b7e1cd928010c56015df2
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-18 09:36:42 -0400

    Updated comments

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // implement your API here:
 
-// Using require() to import express module:
-// ES2015: 
-  // import express from 'express';
+// ES2015 import method: import express from 'express';
+// Importing using require() method:
 const express = require('express');
 const db = require('./data/db');
 
-// Creates an express application (used to configure server):
+// Creating an express application (used to configure server):
 const server = express();
-// ADD THIS LINE TO READ REQUEST BODIES:
-server.use(express.json());
+server.use(express.json()); // ADD THIS LINE TO READ REQUEST BODIES...
 
 // Setting up route handler functions on endpoints:
+
 // POST: Creates a user using the information sent inside the `request body`.
 server.post(`/api/users`,  (req, res) => {
   try {
     const userData = req.body
     if (userData.name.length === 0 || userData.bio.length === 0) {
+      // 400 => BAD REQUEST
       res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
     } else {
       db.insert(userData)
-      // 201 CREATED
+      // 201 => CREATED
       res.status(201).json(userData) 
     }
   } catch(error) {
-    // 505 INTERNAL SERVER ERROR
+    // 505 => INTERNAL SERVER ERROR
     res.status(500).json({ error: "There was an error while saving the user to the database" })
   }
 })
@@ -32,6 +32,7 @@ server.post(`/api/users`,  (req, res) => {
 // GET: returns an array of all the user objects contained in the database. 
 server.get('/api/users', (req, res) => {
   try {
+    // 200 => OK
     res.status(200).json(db.find())
   } catch(error) {
     res.status(500).json({ error: "The users' information could not be retrieved." })
@@ -39,12 +40,11 @@ server.get('/api/users', (req, res) => {
 })
 
 // GET: Returns the user object with the specified `id`. 
-
 server.get('/api/users/:id', (req, res) => {
   try {
     const foundUser = db.findById(req.body.id)
     if (foundUser.length === 0) {
-      // 404 NOT FOUND
+      // 404 => NOT FOUND
       res.status(404).json({ message: "The user with the specified ID does not exist." })
     } else {
       res.status(200).json(foundUser)
@@ -53,6 +53,8 @@ server.get('/api/users/:id', (req, res) => {
     res.status(500).json({ error: "The user information could not be retrieved." })
   }
 })
+
+// DELETE: Removes the user with the specified `id` and returns the deleted user.
 
 // Turning on the server:
 server.listen(5000, () =>
